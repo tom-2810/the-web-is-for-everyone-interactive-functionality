abe9cae839410193d695afa43098ebd47e77d45b
tom-2810 <tom.poortman@hva.nl>
2023-03-31 09:17:53 +0200

    progressive enhancement form

--- public/scripts/dialog.js
+++ public/scripts/dialog.js
@@ -2,6 +2,13 @@ const newProjectDialog = document.querySelector("#new-project-dialog");
 const showDialogButton = document.querySelector("#new-project-button");
 const closeDialog = newProjectDialog.querySelector("#close");
 
+const formHTML = document.querySelector(".filter-section form");
+
+formHTML.hidden = true;
+newProjectDialog.hidden = false;
+showDialogButton.hidden = false;
+
+
 showDialogButton.addEventListener("click", () => {
     newProjectDialog.showModal();
 });
