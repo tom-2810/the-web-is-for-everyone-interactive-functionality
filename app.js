67ad5d350262caac284316fc12f6710bc502b676
tom-2810 <tom.poortman@hva.nl>
2023-04-05 14:15:10 +0200

    Open any project in Toolboard with progression

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const baseUrl = 'https://api.vervoerregio-amsterdam.fdnd.nl/api/v1'
 
 
 const allProjectsData = await fetchJson(`${baseUrl}/urls?first=100`).then((data) => data);
-const urlChecksData = await fetchJson(`${baseUrl}/url?id=clf7zms5va5670bw8rb7gwll2`).then((data) => data);
+// const urlChecksData = await fetchJson(`${baseUrl}/url?id=clf7zms5va5670bw8rb7gwll2`).then((data) => data);
 const websitesData = await fetchJson(`${baseUrl}/websites`).then((data) => data);
 // const checksData = await fetchJson(`${baseUrl}/checks`).then((data) => data);
 const principesData = await fetchJson(`${baseUrl}/principes`).then((data) => data);
@@ -35,8 +35,19 @@ app.get('/', (request, response) => {
 // Route voor het toolboard
 app.get('/toolboard', function (request, response) {
 
-  console.log("de checks van clf7zms5va5670bw8rb7gwll2: " + JSON.stringify(urlChecksData.url.checks[0].succescriteria));
-    response.render('toolboard', { checkedProjectSuccescriteria: urlChecksData.url.checks[0].succescriteria, principesData: principesData, active: '/toolboard' })
+  let { id } = request.query
+
+  if (id) {
+    fetchJson(`${baseUrl}/url?id=${id}`).then((data) => {
+      if (data.url.checks.length != 0) {
+        response.render('toolboard', { checkedProjectSuccescriteria: data.url.checks[0].succescriteria, principesData: principesData, active: '/toolboard' })
+      } else {
+        response.render('toolboard', { checkedProjectSuccescriteria: data.url.checks, principesData: principesData, active: '/toolboard' })
+      }
+    });
+  } else {
+    response.redirect('/projects')
+  }
 })
 
 // Route voor contact
@@ -47,22 +58,25 @@ app.get('/contact', function (request, response) {
 })
 
 // Route voor projects
+/**
+ * @author Luuk Brauckmann
+ */
 app.get('/projects', function (request, response) {
   const sortOptions = [
-    { label: 'Recent', field: 'createdAt', direction: 'DESC'},
-    { label: 'Partner A-Z', field: 'titel', direction: 'ASC'},
-    { label: 'Partner Z-A', field: 'titel', direction: 'DESC'}
+    { label: 'Recent', field: 'createdAt', direction: 'DESC' },
+    { label: 'Partner A-Z', field: 'titel', direction: 'ASC' },
+    { label: 'Partner Z-A', field: 'titel', direction: 'DESC' }
   ]
 
   let { sort } = request.query
   if (sort) sort = JSON.parse(sort)
   else sort = sortOptions[0]
 
-  let urls = [ ...allProjectsData.urls ];
+  let urls = [...allProjectsData.urls];
   urls = urls.map((url) => ({ ...url, titel: url.website.titel }))
   urls = urls.sort((a, b) => a[sort.field].localeCompare(b[sort.field]))
   if (sort.direction === 'DESC') urls.reverse()
-  
+
   response.render('projects', { sortOptions, currentSort: sort, websitesData: websitesData.websites, allProjectsData: urls, active: '/projects' })
 })
 
